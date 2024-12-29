31417cc0fe3d64703482f34de45610ec4d28416c
yoavgur <63050348+yoavgur@users.noreply.github.com>
2024-12-29 11:33:42 +0000

    Deploying to gh-pages from @ yoavgur/yoavgur.github.io@3183b6081bfe12332c67b2843b703c69a98da2ea 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
-          },
         },{id: "nav-submenus",
           title: "submenus",
           description: "",
