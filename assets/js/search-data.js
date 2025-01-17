7b1a952bbfd2209f498a78f46789605f7e6ebc32
yoavgur <63050348+yoavgur@users.noreply.github.com>
2025-01-17 06:47:29 +0000

    Deploying to gh-pages from @ yoavgur/yoavgur.github.io@d1e2d73437112e4c633a8111b39aacf2a0734d88 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,7 +23,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-a-post-with-image-galleries",
+        },{id: "post-using-sparse-autoencoders-for-knowledge-erasure",
+      
+        title: "Using Sparse Autoencoders for Knowledge Erasure",
+      
+      description: "Can we leverage SAEs to effectively erase knowledge from LLMs in a targeted way?",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2025/sae-knowledge-erasure/";
+        
+      },
+    },{id: "post-a-post-with-image-galleries",
       
         title: "a post with image galleries",
       
