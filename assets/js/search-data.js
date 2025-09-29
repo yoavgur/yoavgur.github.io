6933364d8e3f7eee1c954a462928943753e54dc4
yoavgur <63050348+yoavgur@users.noreply.github.com>
2025-09-29 12:15:02 +0000

    Deploying to gh-pages from @ yoavgur/yoavgur.github.io@19ace6ac844f5a4ccb9ed659fb002656875fdb76 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -38,17 +38,6 @@ ninja.data = [{
       section: "Posts",
       handler: () => {
         
-          window.location.href = "/blog/2025/mixing-mechs2/";
-        
-      },
-    },{id: "post-how-language-models-retrieve-bound-entities-in-context",
-      
-        title: "How Language Models Retrieve Bound Entities In-Context",
-      
-      description: "To reason, LMs must bind together entities in-context. How they do this is more complicated than was first thought.",
-      section: "Posts",
-      handler: () => {
-        
           window.location.href = "/blog/2025/mixing-mechs/";
         
       },
