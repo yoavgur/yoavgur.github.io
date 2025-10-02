de4d286ae1c00b9665a5e2045f4e6d90f6acec12
yoavgur <63050348+yoavgur@users.noreply.github.com>
2025-10-02 14:02:55 +0000

    Deploying to gh-pages from @ yoavgur/yoavgur.github.io@ca374fb26bdcd8856d937692a5a1b15ed02cfca2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -63,30 +63,4 @@ ninja.data = [{
           window.location.href = "/blog/2025/sae-knowledge-erasure/";
         
       },
-    },{
-      id: 'light-theme',
-      title: 'Change theme to light',
-      description: 'Change the theme of the site to Light',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("light");
-      },
-    },
-    {
-      id: 'dark-theme',
-      title: 'Change theme to dark',
-      description: 'Change the theme of the site to Dark',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("dark");
-      },
-    },
-    {
-      id: 'system-theme',
-      title: 'Use system default theme',
-      description: 'Change the theme of the site to System Default',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("system");
-      },
     },];
